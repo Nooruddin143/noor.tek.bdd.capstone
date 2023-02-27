b01f5348e19405ded0d2d715eb518a8362be66ad
nooruddin <nooruddin.mohammadi@tekschool.us>
2023-02-27 11:59:52 -0800

    this is the first part of capstone project

---
+++ src/main/java/noor/tek/bdd/config/Browser.java
@@ -0,0 +1,8 @@
+package noor.tek.bdd.config;
+
+import org.openqa.selenium.WebDriver;
+
+public interface Browser {
+	WebDriver openBrowser(String url);
+
+}
---
+++ src/main/java/noor/tek/bdd/config/ChromeHeadless.java
@@ -0,0 +1,20 @@
+package noor.tek.bdd.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+
+public class ChromeHeadless implements Browser {
+	public WebDriver openBrowser(String url) {
+		ChromeDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(options);
+		driver.get(url);
+		return driver;
+
+	}
+
+}
