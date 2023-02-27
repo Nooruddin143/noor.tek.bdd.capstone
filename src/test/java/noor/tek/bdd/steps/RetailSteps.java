98241a189552440cae03ba517870e6575525272b
nooruddin <nooruddin.mohammadi@tekschool.us>
2023-02-27 14:07:22 -0800

    this is the third feature of project

---
+++ src/test/java/noor/tek/bdd/steps/RetailSteps.java
@@ -0,0 +1,9 @@
+package noor.tek.bdd.steps;
+
+import noor.tek.bdd.pages.POMFactory;
+import noor.tek.bdd.utilities.CommonUtility;
+
+public class RetailSteps extends CommonUtility{
+
+	private POMFactory factory = new POMFactory();
+}
